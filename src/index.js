eed5883a8ece80f10d9565e1e3e858ff80a0fd34
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-12 01:22:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import slides from './data.js';
 
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+ReactDOM.render(<App slides={slides} id={1}/>, document.getElementById('root'));
+
+
