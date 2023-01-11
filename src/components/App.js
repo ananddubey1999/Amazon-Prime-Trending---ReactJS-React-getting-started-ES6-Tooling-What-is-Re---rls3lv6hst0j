ae77af7dfc839f2e50cf700a0c55d15fb58df1d5
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-12 01:21:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = ({slides}) => {
+
+  const [index,setIndex]=useState(0);
+
+  
   return (
-    <></>
-  )
+        <div>
+         <div id='slide'>
+           <h1 data-testid="title">{index} : {slides[index].title}</h1>
+           <p data-testid="text">{slides[index].text}</p>
+         </div>
+         <div id='navigation'>
+            <button disabled={index===0} onClick={()=>setIndex(index-1)}>Prev</button>
+            <button disabled={index===slides.length-1} onClick={()=>setIndex(index+1)}>Next</button>
+            <button disabled={index===0} onClick={()=>setIndex(0)}>Restart</button>
+         </div>
+        </div>
+      )
 }
-
-
-export default App;
