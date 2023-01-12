a120f9e68e863d7801e986448aa132d8ea187e63
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-12 19:33:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,63 @@
-import React, {Component, useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../styles/App.css';
 
 const App = ({slides}) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [prevFlag, setPrevFlag] = useState(true);
+  const [nextFlag, setNextFlag] = useState(false);
+  const [restartFlag, setRestartFlag] = useState(true);
 
-  const [index,setIndex]=useState(0);
+  useEffect(() => {
+    // if current slide is the first one
+    if(slideIndex === 0) {
+      setPrevFlag(true);
+      setRestartFlag(true);
+    } else {
+      setPrevFlag(false);
+      setRestartFlag(false);
+    }
 
-  
+    // if current slide is the last one
+    if(slideIndex === slides.length - 1) {
+      setNextFlag(true);
+    } else {
+      setNextFlag(false);
+    }
+
+  }, [slideIndex])
+
+  const handlePrevClick = () => {
+      
+      setSlideIndex((prevState) => prevState - 1);
+  }
+
+  const handleNextClick = () => {
+      setSlideIndex((prevState) => prevState + 1);
+  }
+
+  const handleRestartClick = () => {
+    setSlideIndex(0);
+  }
+
+
+
   return (
+    <>
+      <h1 id="heading">Amazon Prime: Trending</h1>
+      <div>
         <div>
-         <div id='slide'>
-           <h1 data-testid="title">{index} : {slides[index].title}</h1>
-           <p data-testid="text">{slides[index].text}</p>
-         </div>
-         <div id='navigation'>
-            <button disabled={index===0} onClick={()=>setIndex(index-1)}>Prev</button>
-            <button disabled={index===slides.length-1} onClick={()=>setIndex(index+1)}>Next</button>
-            <button disabled={index===0} onClick={()=>setIndex(0)}>Restart</button>
-         </div>
+          <h1 data-testid="title">{slides[slideIndex].title}</h1>
+          <p data-testid="text">{slides[slideIndex].text}</p>
         </div>
-      )
+        
+        <button data-testid="button-prev" onClick={handlePrevClick} disabled={prevFlag}>Prev</button>
+        <button data-testid="button-next" onClick={handleNextClick} disabled={nextFlag}>Next</button>
+        <button data-testid="button-restart" onClick={handleRestartClick} disabled={restartFlag}>Restart</button>
+    
+      </div>
+    </>
+  )
 }
-
 
 
 export default App;
